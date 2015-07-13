7c493c6486b966f64e48f67eaca2f96603289e56
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2015-07-13 22:30:54 +0300

    new list command algorithm
    reworked tree based algorithm for printing list of channels

--- kotik.js
+++ kotik.js
@@ -46,42 +46,64 @@
         }
 
         // !moveto [channel id]: move to channel
-	var search = e.Message.match(/^!moveto[ ]?(\d+)/);
-	if (search) {
-	    piepan.Self.Move(piepan.Channels[search[1]]);
+		var search = e.Message.match(/^!moveto[ ]?(\d+)/);
+		if (search) {
+		    piepan.Self.Move(piepan.Channels[search[1]]);
         }
+
+        var printChildChannels = new function(out,channel,level){
+        	for(var key in channel){
+       			for (var i = 0; i <level; i++) {
+        			out+='-'; 		
+        		}
+        		out+=channel[key].ID + ": " + channel[key].Name+"<br/>";
+        		if(channel[key].Children.length > 0){
+					printChildChannels(out,channel[key].Children,level+1);
+				}
+        	}
+        };
 		
-	// !list: list channels
-	var search = e.Message.match(/^!list/);
-	if (search) {
-            var channels="";
-	    for (var key in piepan.Channels) {
-		channels+=piepan.Channels[key].ID + ": " + piepan.Channels[key].Name+"<br/>";
-	    }
+		// !list: list channels
+		var search = e.Message.match(/^!list/);
+		var channels="";
+		if (search) {
+			Channel root = piepan.Channels.Find("Root");
+			if(root != null){
+				channels+=root.ID + ": " + root.Name+"<br/>";
+				if(root.Children.length > 0){
+					printChildChannels(channels,root.Children,1);
+				}
+			}
+		    /*for (var key in piepan.Channels) {
+				channels+=piepan.Channels[key].ID + ": " + piepan.Channels[key].Name+"<br/>";
+				if(piepan.Channels[key].Children.length > 0){
+					printChildChannels(channels,piepan.Channels[key].Children,1);
+				}
+		    }*/
             e.Sender.Send(channels);
+    	}
+		
+		// !stop: stop audio
+		var search = e.Message.match(/^!stop/);
+		if (search) {
+		    piepan.Audio.Stop();
         }
 		
-	// !stop: stop audio
-	var search = e.Message.match(/^!stop/);
-	if (search) {
-	    piepan.Audio.Stop();
+		// !help: display help message
+		var search = e.Message.match(/^!help/);
+		if (search) {
+			e.Sender.Send("#[sound]     : воспроизводит звук. Доступные звуки: meow|gold|farm|dryad|purr|sergey|idisuda|podcast|perfectplan|nedarom|netak|rest|leeroy|murloc");
+			e.Sender.Send("!stop        : останавливает текущий звук");
+			e.Sender.Send("!list        : список каналов");
+			e.Sender.Send("!moveto [id] : перейти на канал");
+			e.Sender.Send("!help        : выводит справку");
+            e.Sender.Send("!volume [fl] : устанавливает громкость. Дефолт: 1.0");
+		}
+		
+        // !volume: change volume, default is 1.0
+        var search = e.Message.match(/^!volume[ ]?(\d+[.]?[\d]?)/);
+        if (search) {
+            piepan.Audio.SetVolume(search[1]);
         }
-		
-	// !help: display help message
-	var search = e.Message.match(/^!help/);
-	if (search) {
-		e.Sender.Send("#[sound]     : воспроизводит звук. Доступные звуки: meow|gold|farm|dryad|purr|sergey|idisuda|podcast|perfectplan|nedarom|netak|rest|leeroy|murloc");
-		e.Sender.Send("!stop        : останавливает текущий звук");
-		e.Sender.Send("!list        : список каналов");
-		e.Sender.Send("!moveto [id] : перейти на канал");
-		e.Sender.Send("!help        : выводит справку");
-                e.Sender.Send("!volume [fl] : устанавливает громкость. Дефолт: 1.0");
-	}
-		
-       // !volume: change volume, default is 1.0
-       var search = e.Message.match(/^!volume[ ]?(\d+[.]?[\d]?)/);
-       if (search) {
-                piepan.Audio.SetVolume(search[1]);
-       }
     });
 })();
